454ac04d62ead8101d4f2150723e03def64a6b1f
Shane Rogers <swrogers@gmail.com>
2019-08-22 16:24:56 -0500

    Site updated

---
+++ content/first.md
@@ -0,0 +1,19 @@
+Title: First Post
+Date: 2019-08-22
+Category: Blog
+Tags: pelican, other
+Slug: first-post
+Authors: Shane Rogers
+Summary: This is the first.
+ 
+ 
+ 
+The First Post
+================
+ 
+Here we go again...
+ 
+### Header 3
+ 
+> Is this working?
+> Because I don't think so
---
+++ pelicanconf.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Shane Rogers'
+SITENAME = "Shane's Blog"
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+PLUGIN_PATHS = ['pelican-plugins']
+
+PLUGINS = [
+    'i18n_subsites',
+]
+
+JINJA_ENVIRONMENT = {
+        'extensions': ['jinja2.ext.i18n']
+}
+
+
+THEME = 'pelican-themes/pelican-bootstrap3'
+BOOTSTRAP_THEME = 'darkly'
+
+TYPOGRIFY = True
